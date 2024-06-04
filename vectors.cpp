8149b1d8904b516de31a597f051ccc1b3696a460
suresh.vakude <sureshvakude8906@gmail.com>
2024-06-05 00:38:42 +0530

    20240605A - vectors

--- vectors.cpp
+++ vectors.cpp
@@ -23,6 +23,45 @@
 using namespace std;
 
 int main(){
-    
+    vector<int> vec;
+    vector<int> temp({2,15,16,68});
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+    vec.push_back(40);
+
+    cout<<"push back: ";
+    for(auto v : vec) cout<<v<<" ";
+    cout<<endl;
+
+    vec.pop_back();
+    cout<<"pop back: ";
+    for(auto v : vec) cout<<v<<" ";
+    cout<<endl;
+
+    cout<<"size: "<<vec.size()<<endl;
+
+    cout<<"empty: "<<vec.empty()<<endl;
+
+    vec.erase(vec.begin());
+    cout<<"erase(vec.begin()): ";
+    for(auto v : vec) cout<<v<<" ";
+    cout<<endl;
+
+    vec.erase(vec.begin(),vec.end());
+    cout<<"erase(vec.begin(),vec.end()): ";
+    for(auto v : vec) cout<<v<<" ";
+    cout<<endl;
+
+    vec.insert(vec.begin(),temp.begin(),temp.end());
+    cout<<"insert: ";
+    for(auto v : vec) cout<<v<<" ";
+    cout<<endl;
+
+    vec.clear();
+    cout<<"clear: ";
+    for(auto v : vec) cout<<v<<" ";
+    cout<<endl;
+
     return 0;
 }
