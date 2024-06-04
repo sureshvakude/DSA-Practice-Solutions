10a892475d16a401c0e8578729480ca41a7ba3d2
suresh.vakude <suresh.vakude@acty-sys.co.in>
2024-06-04 18:13:54 +0530

    20240604B - vectors

---
+++ vectors.cpp
@@ -0,0 +1,28 @@
+/*
+
+## Ways to declare the vector :
+    1. vector<dataType> vectorName;
+    2. vector<dataType> name({ value1, value2, value3 ....}); 
+    3. vector<dataType> name(size, value);
+    4. vector<dataType> name(other_vec);
+
+# It have iterator too. 
+
+## Imp Methods
+    push_back() 
+    pop_back() 
+    size() 
+    empty() 
+    insert() 
+    erase() 
+    clear() 
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    return 0;
+}
