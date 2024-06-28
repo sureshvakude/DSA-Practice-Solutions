a066a5b8318b418519d7a431448000b2f6ca6e05
suresh.vakude <suresh.vakude@acty-sys.co.in>
2024-06-28 17:58:10 +0530

    20240628A - map

--- 7_map.cpp
+++ 7_map.cpp
@@ -9,7 +9,22 @@ Types of map:
 1. map:
 # Ways to declare map:
     1. map<int,int> mp;
+    2. map<int,string> mp = {{1,"one"},{2,"two"}};
+    3. map<int,string,customCap> mp;
 
+## It have iterators too.
+
+## Imp Methods:
+    size()
+    empty()
+    pair_insert()
+    erase()
+    clear()
+    insert()
+    lower_bound()
+    upper_bound()
+    count()
+    
 
 */
 
